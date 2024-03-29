1a2c76a9517cd122bfd4832f95ce4c4d0a358381
Samuel Nnanna <samuelnnanna71@gmail.com>
2024-03-29 19:05:26 +0100

    adjusted configurations

--- buildscript/change_permission.sh
+++ buildscript/change_permission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Change directory and change ownership
-cd /var/www/html/ && sudo chown -R www-data:ubuntu kubernetes-setup/
+# Change directory and change ownership
+cd /var/www/html/kubernetes-setup/ && sudo chown -R www-data:ubuntu .
 
-#change directory and change permission
-cd /var/www/html/ && sudo chmod -R 770 kubernetes-setup/
+# Change directory and change permissions
+cd /var/www/html/kubernetes-setup/ && sudo chmod -R 770 .
